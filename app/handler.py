8ec75de374febcbb3ea47d102686eaeee4e94b0e
Tyler <4926565+tyler71@users.noreply.github.com>
2024-10-08 22:12:37 -0700

    Use nested directories (segments) and check each directory in there for valid domains. This is to allow jailing and ftp access

--- app/handler.py
+++ app/handler.py
@@ -3,16 +3,16 @@ import logging
 
 from watchdog.events import FileSystemEventHandler
 
-
 class HostnameChangeHandler(FileSystemEventHandler):
-    def __init__(self, directory):
+    def __init__(self, directory, segment_set: set[str], domain_set: set[str]):
         self.monitored_directory = directory
-        self.valid_hostnames = self.scan_directory()
+        self.valid_hostnames = domain_set
+        self.valid_hostnames |= self.scan_directory()
+        self.segment_set = segment_set
 
     def scan_directory(self) -> set[str]:
-        self.valid_hostnames = set((item for item in os.listdir(self.monitored_directory)
-                                    if os.path.isdir(os.path.join(self.monitored_directory, item))))
-        return self.valid_hostnames
+        return set((item for item in os.listdir(self.monitored_directory)
+                    if os.path.isdir(os.path.join(self.monitored_directory, item))))
 
     def check_hostname(self, hostname: str) -> bool:
         return hostname in self.valid_hostnames
@@ -24,13 +24,13 @@ class HostnameChangeHandler(FileSystemEventHandler):
             logging.info(f'New hostname added: {impacted_dir}')
 
     def on_deleted(self, event) -> None:
-        if event.is_directory:
+        if event.is_directory and event.src_path not in self.segment_set:
             impacted_dir = os.path.basename(event.src_path)
             self.valid_hostnames.remove(impacted_dir)
             logging.info(f'Hostname deleted: {impacted_dir}')
 
     def on_moved(self, event) -> None:
-        if event.is_directory:
+        if event.is_directory and event.src_path not in self.segment_set:
             original = os.path.basename(event.src_path)
             target = os.path.basename(event.dest_path)
             self.valid_hostnames.remove(original)
--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
+import logging
 import os
+from pathlib import Path
 
 from quart import Quart, request, jsonify
 from watchdog.observers import Observer
@@ -19,20 +21,33 @@ INVALID_DOMAIN_HTTP_RESPONSE = 403
 
 app = Quart(__name__)
 
-hostname_change_handler = HostnameChangeHandler(directory=monitored_directory)
-hostname_change_handler.scan_directory()
+p = Path(monitored_directory)
+directories = (x for x in p.iterdir() if x.is_dir())
+
+# All directories. So /site/A, /site/B. This would be A, B
+segment_set = set()
+
+# All domains, so /site/A/apple.com, would be apple.com
+domain_set = set()
 
 observer = Observer()
-observer.schedule(hostname_change_handler, monitored_directory, recursive=False)
+for directory in directories:
+    abs_dir = directory.absolute()
+    segment_set.add(directory.name)
+    hostname_change_handler = HostnameChangeHandler(directory=abs_dir, domain_set=domain_set, segment_set=segment_set)
+    observer.schedule(hostname_change_handler, abs_dir, recursive=False)
+
 observer.start()
 
+logging.info(f'Registered segments: {segment_set}')
+app.logger.info(f"monitored directory: {monitored_directory}")
 app.logger.info(f'Permitted domains: {" ".join(hostname_change_handler.valid_hostnames)}')
 
 
 @app.route('/validate', methods=['GET'])
 def main():
     domain = request.args.get('domain')
-    if hostname_change_handler.check_hostname(domain):
+    if domain in domain_set:
         return jsonify({'message': f'Domain {domain} is valid'}), VALID_DOMAIN_HTTP_RESPONSE
     else:
         return jsonify({'message': f'Domain {domain} is not valid'}), INVALID_DOMAIN_HTTP_RESPONSE
